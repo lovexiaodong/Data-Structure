2b29ab134627895d0521b2e057c310760f4bb564
william.zhang <zydqlg@sina.com>
2017-02-23 18:14:15 +0800

    数据结构之链表

---
+++ DataStructure/src/com/zyd/entity/NodeTest.java
@@ -0,0 +1,12 @@
+package com.zyd.entity;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class NodeTest {
+
+	public static void main(String[] args) {
+		
+		List list = new LinkedList<String>();
+	}
+}
---
+++ DataStructure/src/com/zyd/entity/StructNode.java
@@ -0,0 +1,7 @@
+package com.zyd.entity;
+
+public class StructNode<T> {
+
+	public T t;
+	public StructNode<T> next;
+}
